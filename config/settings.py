6e28805972ea66f32f54de568e02c3b8e86ff765
Luis <lcanafistula.br@gmail.com>
2025-02-28 19:53:37 -0300

    Corrigindo erros no vercel

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'config/static')
-]
+]   
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
